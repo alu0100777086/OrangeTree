ee789a6cb3be0db11abba844778b9c84216f0f1d
Marcos Luis <alu0100777086@ull.edu.es>
2014-12-18 22:20:36 +0000

    Rspec and more files

---
+++ lib/orangetree.rb
@@ -0,0 +1,5 @@
+require "orangetree/version"
+
+module Orangetree
+  # Your code goes here...
+end
---
+++ spec/oragetree_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'orangetree'
+
+describe Orangetree do
+	it 'requires additional testing'
+end
